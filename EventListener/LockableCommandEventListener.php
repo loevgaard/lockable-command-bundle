3e23856723f0cb8c1a637060d3d2227939922afa
Joachim Løvgaard <joachim@loevgaard.dk>
2017-02-01 10:44:46 +0100

    Going back to loevgaard/locker

--- EventListener/LockableCommandEventListener.php
+++ EventListener/LockableCommandEventListener.php
@@ -2,46 +2,39 @@
 namespace Loevgaard\LockableCommandBundle\EventListener;
 
 use Loevgaard\LockableCommandBundle\Command\LockableCommandInterface;
+use Loevgaard\Locker\FileLocker;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Event\ConsoleEvent;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\Console\Output\Output;
-use Symfony\Component\Filesystem\LockHandler;
 
 class LockableCommandEventListener
 {
     public function onConsoleCommand(ConsoleCommandEvent $event) {
         $event->getOutput()->writeln('Trying to acquire lock...', Output::VERBOSITY_VERBOSE);
         $locker = $this->getLocker($event);
-        if($locker) {
-            $locked = $locker->lock();
-            if($locked) {
-                $event->getOutput()->writeln('Lock acquired...', Output::VERBOSITY_VERBOSE);
-            } else {
-                $event->getOutput()->writeln('Command locked, disabling command...', Output::VERBOSITY_VERBOSE);
-                $event->disableCommand();
-            }
+        if($locker && !$locker->lock()) {
+            $event->getOutput()->writeln('Command locked, disabling command...', Output::VERBOSITY_VERBOSE);
+            $event->disableCommand();
         }
     }
 
     public function onConsoleTerminate(ConsoleTerminateEvent $event) {
         $locker = $this->getLocker($event);
-        if($locker) {
-            $locker->release();
-        }
+        $locker && $locker->release();
     }
 
     /**
      * @param ConsoleEvent $event
-     * @return LockHandler|null
+     * @return FileLocker|null
      */
     private function getLocker(ConsoleEvent $event) {
         $command        = $event->getCommand();
-        $lockHandler    = null;
+        $locker    = null;
 
         if($command instanceof LockableCommandInterface) {
-            $lockHandler = new LockHandler($command->getName());
+            $locker = new FileLocker($command->getName());
         }
-        return $lockHandler;
+        return $locker;
     }
 }
