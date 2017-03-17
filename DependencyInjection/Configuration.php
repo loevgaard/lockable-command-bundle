e7fadf9e2f2bd240be1d4e9ddf782c7d1d05449d
Joachim Løvgaard <joachim@loevgaard.dk>
2017-03-17 15:16:32 +0100

    added config option for lock dir

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+namespace Loevgaard\LockableCommandBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('loevgaard_lockable_command');
+
+        $rootNode
+            ->children()
+                ->scalarNode('lock_dir')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- DependencyInjection/LoevgaardLockableCommandExtension.php
+++ DependencyInjection/LoevgaardLockableCommandExtension.php
@@ -10,10 +10,12 @@ class LoevgaardLockableCommandExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter('loevgaard_lockable_command.lock_dir', $config['lock_dir']);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 }
